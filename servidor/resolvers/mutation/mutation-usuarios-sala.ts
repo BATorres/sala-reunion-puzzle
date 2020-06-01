9dd78587bb7dd11b0c53751f57344aaa0f649d1d
Andrés Torres Albuja <bryan.torres01@epn.edu.ec>
2020-05-31 19:08:06 -0500

    Mutation gpara acciones de usuario en sala

--- servidor/resolvers/mutation/mutation-usuarios-sala.ts
+++ servidor/resolvers/mutation/mutation-usuarios-sala.ts
@@ -1,9 +1,9 @@
 import {ContextoInterface} from '../../interfaces/contexto.interface';
 
 export const mutationUsuariosSala = {
-    async pedirLaPalabra(
+    async accionesUsuarioSala(
         padre,
-        {idUsuario, idSala},
+        {idUsuario, idSala, tipoAccion},
         contexto: ContextoInterface
     ) {
         const usuarioSala = await contexto.db.usuarioSalas({
@@ -23,13 +23,39 @@ export const mutationUsuariosSala = {
             }
         });
 
-        return contexto.db.updateUsuarioSala({
-            data: {
-                levantarMano: true
-            },
-            where: {
-                id: usuarioSala[0].id
-            }
-        });
+        if (tipoAccion === 'Pedir la palabra') {
+            return contexto.db.updateUsuarioSala({
+                data: {
+                    levantarMano: true
+                },
+                where: {
+                    id: usuarioSala[0].id
+                }
+            });
+        }
+
+        if (tipoAccion === 'Compartir pantalla') {
+            return contexto.db.updateUsuarioSala({
+                data: {
+                    levantarMano: false,
+                    compartirPantalla: true
+                },
+                where: {
+                    id: usuarioSala[0].id
+                }
+            });
+        }
+
+        if (tipoAccion === 'Cancelar') {
+            return contexto.db.updateUsuarioSala({
+                data: {
+                    levantarMano: false,
+                    compartirPantalla: false
+                },
+                where: {
+                    id: usuarioSala[0].id
+                }
+            });
+        }
     },
 };
