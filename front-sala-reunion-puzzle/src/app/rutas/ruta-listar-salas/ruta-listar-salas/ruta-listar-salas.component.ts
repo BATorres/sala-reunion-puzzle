b61690819d98c06c8f833fb4643f7c3f9447b0f5
Andrés Torres Albuja <bryan.torres01@epn.edu.ec>
2020-07-02 23:29:25 -0500

    Creación de proyecto angular y prueba con endpoint

---
+++ front-sala-reunion-puzzle/src/app/rutas/ruta-listar-salas/ruta-listar-salas/ruta-listar-salas.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {Apollo} from 'apollo-angular';
+import {LISTAR_SALAS} from '../../../constantes/queries';
+
+@Component({
+  selector: 'app-ruta-listar-salas',
+  templateUrl: './ruta-listar-salas.component.html',
+  styleUrls: ['./ruta-listar-salas.component.css']
+})
+export class RutaListarSalasComponent implements OnInit {
+
+  constructor(
+    private readonly apollo: Apollo
+  ) { }
+
+  ngOnInit(): void {
+    this.apollo.watchQuery({
+      query: LISTAR_SALAS
+    }).valueChanges.subscribe(
+      (salas) => {
+        console.log('salas', salas);
+      }
+    )
+  }
+}
--- servidor/interfaces/contexto.interface.ts
+++
@@ -1,6 +0,0 @@
-import {Prisma} from '../generated/prisma-client';
-
-export interface ContextoInterface {
-    db: Prisma
-    request: any
-}
